2438dad508f6a02efff359976514aa221a8e560b
WY <wiangf@hotmail.com>
2019-10-25 09:47:31 +0800

    ??

---
+++ AMS/AMS/Models/ReviewViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMS.Models
+{
+    public enum MyEnum
+    {
+        審核中 = 1,
+        已審核 = 2,
+        
+    }
+}
