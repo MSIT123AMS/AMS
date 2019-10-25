35dfdb0f40353151246f028e2e44e56525f5a3ef
廷睿 <poi820704@gmail.com>
2019-10-25 11:53:02 +0800

    1025 中午

--- AMS/AMS/Metadata/LeaveRequestesMatadata.cs
+++ AMS/AMS/Metadata/LeaveRequestesMatadata.cs
@@ -10,14 +10,14 @@ namespace AMS.Metadata
     {
         public string LeaveRequestID { get; set; }
         public string EmployeeID { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-ddThh:mm}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> RequestTime { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-ddThh:mm}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> StartTime { get; set; }
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-ddThh:mm}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> EndTime { get; set; }
         public string LeaveType { get; set; }
         public string LeaveReason { get; set; }
