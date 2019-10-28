a48b6e284ab9efb8aff80a04878c7b9cbaef5e33
WY <wiangf@hotmail.com>
2019-10-28 17:10:53 +0800

    before merge

--- AMS/AMS/Models/OverTimeViewModel.cs
+++ AMS/AMS/Models/OverTimeViewModel.cs
@@ -19,12 +19,12 @@ namespace AMS.Models
 
         [DataType(DataType.DateTime)]
         [Display(Name = "開始時間")]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/ddTHH:mm}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime StartTime { get; set; }
 
         [DataType(DataType.DateTime)]
         [Display(Name = "結束時間")]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/ddTHH:mm}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime EndTime { get; set; }
 
         [Display(Name = "請假補休")]
@@ -44,7 +44,7 @@ namespace AMS.Models
 
         [DataType(DataType.Date)]
         [Display(Name = "審核時間")]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd} hh:mm", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime? ReviewTime { get; set; }
     }
 
