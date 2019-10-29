8ec3a7e11b240296cd79a10155018d5d0c5eb884
User <User@DESKTOP-H3ECMEV>
2019-10-29 11:52:45 +0800

    EDITDETAILS

--- AMS/AMS/Controllers/EmployeesController.cs
+++ AMS/AMS/Controllers/EmployeesController.cs
@@ -8,6 +8,8 @@ using System.Web;
 using System.Web.Mvc;
 using AMS.Models;
 
+using System.IO;
+
 namespace AMS.Controllers
 {
     public class EmployeesController : Controller
@@ -257,7 +259,7 @@ namespace AMS.Controllers
                    ,
                 Phone = emp.Phone,
                 JobStaus = emp.JobStaus
-
+                , Photo=emp.Photo
             };
             if (emp.gender==true)
             {
@@ -333,6 +335,20 @@ namespace AMS.Controllers
                     , Phone=emp.Phone
             
                 };
+                if (Request.Files["empFile"].ContentLength != 0)
+                {
+                    byte[] data = null;
+                    using (BinaryReader br = new BinaryReader(
+                        Request.Files["empFile"].InputStream))
+                    {
+                        data = br.ReadBytes(Request.Files["empFile"].ContentLength);
+                    }
+                    employees.Photo= data;
+                }
+
+
+
+
                 employees.DepartmentID = db.Departments.Where(e=>e.DepartmentName==emp.DepartmentName).First().DepartmentID;
                 employees.JobStaus = "在職";
 
@@ -379,7 +395,8 @@ namespace AMS.Controllers
                    ,
                 Phone = emp.Phone,
                 JobStaus = emp.JobStaus
-
+                ,
+                Photo = emp.Photo
             };
             if (emp.gender == true)
             {
@@ -393,7 +410,8 @@ namespace AMS.Controllers
             employees.Manager = db.Departments.Where(e => e.DepartmentID == emp.DepartmentID).First().Manager;
 
 
-           
+            return View(employees);
+
             if (employees == null)
             {
                 return HttpNotFound();
@@ -406,15 +424,81 @@ namespace AMS.Controllers
         // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "EmployeeID,EmployeeName,IDNumber,DeputyPhone,Deputy,Marital,Email,Birthday,Leaveday,Hireday,Address,DepartmentID,PositionID,Phone,Photo,JobStaus,JobTitle,EnglishName,gender,Notes,LineID,Education")] Employees employees)
+        public ActionResult Edit([Bind(Include = "DepartmentName,EmployeeID,EmployeeName,IDNumber,DeputyPhone,Deputy,Marital,Email,Birthday,Leaveday,Hireday,Address,DepartmentID,PositionID,Phone,Photo,JobStaus,JobTitle,EnglishName,gender,Notes,LineID,Education")] EmployeesDetailsViewModel emp)
         {
             if (ModelState.IsValid)
             {
+                Employees employees = new Employees
+                {
+
+                    EmployeeID = emp.EmployeeID,
+                    EmployeeName = emp.EmployeeName,
+                    IDNumber = emp.IDNumber,
+                    DeputyPhone = emp.DeputyPhone,
+                    Deputy = emp.Deputy,
+                    Marital = emp.Manager,
+                    Email = emp.Email,
+                    Birthday = emp.Birthday,
+                    Hireday = emp.Hireday,
+                    Address = emp.Address,
+
+                    JobTitle = emp.JobTitle,
+                    EnglishName = emp.EnglishName,
+                   
+                    Notes = emp.Notes,
+                    Education = emp.Education
+                    //, Photo = null
+                    //, LineID = ""
+                    //, Leaveday = null
+                    ,
+                    Phone = emp.Phone
+
+                };
+                if (Request.Files["empFile"].ContentLength != 0)
+                {
+                    byte[] data = null;
+                    using (BinaryReader br = new BinaryReader(
+                        Request.Files["empFile"].InputStream))
+                    {
+                        data = br.ReadBytes(Request.Files["empFile"].ContentLength);
+                    }
+                    employees.Photo = data;
+                }
+
+                if (emp.gender=="男生")
+                {
+                    employees.gender = true;
+                }
+                else
+                {
+                    employees.gender = false;
+                }
+                
+
+                employees.DepartmentID = db.Departments.Where(e => e.DepartmentName == emp.DepartmentName).First().DepartmentID;
+                employees.JobStaus = "在職";
+
                 db.Entry(employees).State = EntityState.Modified;
                 db.SaveChanges();
+
                 return RedirectToAction("Index");
-            }
-            return View(employees);
+
+               
+
+                 
+
+
+
+
+
+
+                }
+            return View(emp);
+
+
+
+
+
         }
 
         // GET: Employees/Delete/5
@@ -450,6 +534,11 @@ namespace AMS.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+        public FileResult ShowPhoto(string id)
+        {
+            byte[] content = db.Employees.Find(id).Photo;
+            return File(content, "image/jpeg");
         }
     }
 }
