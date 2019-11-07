42d09169c9c1d68e174f68032225942822c308e6
hauwei peng <hauwei.pong@gmail.com>
2019-11-07 20:45:07 +0800

    11/07 將打卡申請跟刷卡紀錄修改成singlepage

--- AMS/AMS/Controllers/AttendancesController.cs
+++ AMS/AMS/Controllers/AttendancesController.cs
@@ -41,32 +41,12 @@ namespace AMS.Controllers
 
 
 
-            return View(query);
+            return PartialView("_SerchAttendances", query);
 
             //return View();
         }
 
-        [HttpPost]
-        public ActionResult SerchAttendances(DateTime time1, DateTime time2)
-        {
-            string EmployeeID = "MSIT1230005";
-            var query = db.Attendances.Where(Att => Att.EmployeeID == EmployeeID && Att.Date>=time1 && Att.Date<=time2).Join(db.Employees, Attendances => Attendances.EmployeeID, Employees => Employees.EmployeeID, (Attendances, Employees) => new AttendancesViewModel {
-               EmployeeID=Attendances.EmployeeID,
-                EmployeeName=Employees.EmployeeName,
-                Date= Attendances.Date,
-                OnDuty=Attendances.OnDuty,
-                OffDuty= Attendances.OffDuty,
-                station=Attendances.station
-            });
-             
-
-
-
-
-            return View(query);
-
-            //return View();
-        }
+      
         [HttpPost]
         public ActionResult SerchAttendances(DateTime time1,DateTime time2)
         {
