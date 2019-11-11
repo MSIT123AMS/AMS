79494f1b8eb1e64006e92689fd10a259875a1233
User <User@DESKTOP-H3ECMEV>
2019-11-11 11:37:58 +0800

    1107 singlepage undone

--- AMS/AMS/Controllers/OverTimeRequestsController.cs
+++ AMS/AMS/Controllers/OverTimeRequestsController.cs
@@ -43,7 +43,7 @@ namespace AMS.Controllers
 
 
 
-            return View(overTimeRequest);
+            return PartialView("_Index",overTimeRequest);
         }
 
         
@@ -123,7 +123,7 @@ namespace AMS.Controllers
                 return HttpNotFound();
             }
 
-            return View(overTimeRequest);
+            return PartialView("_Details",overTimeRequest);
         }
             public class PayOff
         {
@@ -142,7 +142,7 @@ namespace AMS.Controllers
 
             };
             ViewBag.OverTimePay = new SelectList(dropdownlist, "value", "text");
-            return View();
+            return PartialView("_Create");
         }
 
         // POST: OverTimeRequests/Create
@@ -152,86 +152,68 @@ namespace AMS.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "StartTime,EndTime,OverTimePay,OverTimeReason")] OverTimeRequest overTimeRequest)
         {
+
             string User = Convert.ToString(Session["UserName"]);
             if (ModelState.IsValid)
             {
-                overTimeRequest.OverTimeRequestID = "999";
+                var Query = (from ot in db.OverTimeRequest.AsEnumerable()
+                                       join emp in db.Employees.AsEnumerable() on ot.EmployeeID equals emp.EmployeeID
+                                       join rev in db.ReviewStatus.AsEnumerable() on ot.ReviewStatusID equals rev.ReviewStatusID
+                                       join date in db.WorkingDaySchedule.AsEnumerable() on ot.StartTime.Date equals date.Date
+                                       where ot.EmployeeID == User
+                                       select new OverTimeViewModel
+                                       {
+                                           RequestID = ot.OverTimeRequestID,
+                                           EmployeeName = emp.EmployeeName,
+                                           RequestTime = ot.RequestTime,
+                                           StartTime = ot.StartTime,
+                                           EndTime = ot.EndTime,
+                                           PayorOFF = OvertimeObj.PayorOff(ot.OverTimePay),
+                                           OTDateType = date.WorkingDay,
+                                           SummaryTime = OvertimeObj.Summary(ot.StartTime, ot.EndTime, date.WorkingDay, ot.OverTimePay),
+                                           Reason = ot.OverTimeReason,
+                                           Review = rev.ReviewStatus1,
+                                           ReviewTime = ot.ReviewTime
+                                       });
+                var LeaveQuery = from leave in db.LeaveRequests
+                                 where leave.EmployeeID == User
+                                 select leave;
+                                 
+                if(Query.Any(n=>n.StartTime.Date==overTimeRequest.StartTime.Date&&!(n.Review=="3")))
+                {
+                    //重複加班
+                }
+                if(LeaveQuery.Any(n=>n.StartTime== overTimeRequest.StartTime.Date &&!(n.ReviewStatusID==3)))
+                {
+                    //請假
+                }
+                    
+                    
+                    
+                    
+
+
+
+
+
+
+
+                overTimeRequest.OverTimeRequestID = Convert.ToString(Query.Count()+1);
                 overTimeRequest.EmployeeID = User;
                 overTimeRequest.RequestTime = DateTime.Now;
                 overTimeRequest.ReviewStatusID = 1;
                 
                 db.OverTimeRequest.Add(overTimeRequest);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index","Home");
             }
 
-            return View(overTimeRequest);
+            return RedirectToAction("Index", "Home");
         }
 
-        // GET: OverTimeRequests/Edit/5
-        //public ActionResult Edit(string id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    OverTimeRequest overTimeRequest = db.OverTimeRequest.Find(id);
-        //    if (overTimeRequest == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(overTimeRequest);
-        //}
+  
 
-        // POST: OverTimeRequests/Edit/5
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "OverTimeRequestID,EmployeeID,RequestTime,StartTime,EndTime,OverTimePay,OverTimeReason,ReviewStatusID,ReviewTime")] OverTimeRequest overTimeRequest)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(overTimeRequest).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(overTimeRequest);
-        //}
 
-        //// GET: OverTimeRequests/Delete/5
-        //public ActionResult Delete(string id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    OverTimeRequest overTimeRequest = db.OverTimeRequest.Find(id);
-        //    if (overTimeRequest == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(overTimeRequest);
-        //}
 
-        // POST: OverTimeRequests/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(string id)
-        //{
-        //    OverTimeRequest overTimeRequest = db.OverTimeRequest.Find(id);
-        //    db.OverTimeRequest.Remove(overTimeRequest);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
-
-        //protected override void Dispose(bool disposing)
-        //{
-        //    if (disposing)
-        //    {
-        //        db.Dispose();
-        //    }
-        //    base.Dispose(disposing);
-        //}
     }
 }
