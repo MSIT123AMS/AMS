735a632116eebd63e409b6e2c847f00cf09f5ea4
User <User@DESKTOP-H3ECMEV>
2019-11-11 12:48:02 +0800

    1111 singlepageOK

--- AMS/AMS/Controllers/OverTimeRequestsController.cs
+++ AMS/AMS/Controllers/OverTimeRequestsController.cs
@@ -149,7 +149,7 @@ namespace AMS.Controllers
         // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
         // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        //[ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "StartTime,EndTime,OverTimePay,OverTimeReason")] OverTimeRequest overTimeRequest)
         {
 
@@ -187,10 +187,6 @@ namespace AMS.Controllers
                 {
                     //請假
                 }
-                    
-                    
-                    
-                    
 
 
 
@@ -198,7 +194,11 @@ namespace AMS.Controllers
 
 
 
-                overTimeRequest.OverTimeRequestID = Convert.ToString(Query.Count()+1);
+
+
+                
+
+                overTimeRequest.OverTimeRequestID = Convert.ToString(db.OverTimeRequest.Count()+1);
                 overTimeRequest.EmployeeID = User;
                 overTimeRequest.RequestTime = DateTime.Now;
                 overTimeRequest.ReviewStatusID = 1;
