f600651924fc04f74da8bd03e741d232b7d8e563
User <User@DESKTOP-H3ECMEV>
2019-12-12 14:21:50 +0800

    公告通知小鈴鐺

--- AMS/AMS/SignalR/MyHub1.cs
+++ AMS/AMS/SignalR/MyHub1.cs
@@ -14,19 +14,27 @@ namespace AMS.SignalR
     public class MyHub : Hub
     {
         // Is set via the constructor on each creation
-        private Broadcaster _broadcaster;
+        //private Broadcaster _broadcaster;
+        private Entities db = new Entities();
 
 
-        public MyHub()
-            : this(Broadcaster.Instance)
+        //public MyHub()
+        //    : this(Broadcaster.Instance)
+        //{
+
+        //}
+
+        //public MyHub(Broadcaster broadcaster)
+        //{
+        //    _broadcaster = broadcaster;
+
+        //}
+
+        public void Send(string name, string message)
         {
-
-        }
-
-        public MyHub(Broadcaster broadcaster)
-        {
-            _broadcaster = broadcaster;
-
+            Dictionary<string,string> IDtoName = db.Employees.ToDictionary(n => n.EmployeeID, n => n.EmployeeName);
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(IDtoName[name], message);
         }
 
     }
@@ -35,45 +43,45 @@ namespace AMS.SignalR
     /// <summary>
     /// 數據廣播器
     /// </summary>
-    public class Broadcaster
-    {
-        private readonly static Lazy<Broadcaster> _instance =
-            new Lazy<Broadcaster>(() => new Broadcaster());
+    //public class Broadcaster
+    //{
+    //    private readonly static Lazy<Broadcaster> _instance =
+    //        new Lazy<Broadcaster>(() => new Broadcaster());
 
-        private readonly IHubContext _hubContext;
+    //    private readonly IHubContext _hubContext;
 
-        private Timer _broadcastLoop;
+    //    private Timer _broadcastLoop;
 
-        public Broadcaster()
-        {
-            // 獲取所有連接的句柄，方便後面進行消息廣播
-            _hubContext = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
-            // Start the broadcast loop
-            _broadcastLoop = new Timer(
-                BroadcastShape,
-                null,
-                1000,
-                1000);
-            //用SqlDependency Call BroadcastShape
+    //    public Broadcaster()
+    //    {
+    //        // 獲取所有連接的句柄，方便後面進行消息廣播
+    //        _hubContext = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+    //        // Start the broadcast loop
+    //        _broadcastLoop = new Timer(
+    //            BroadcastShape,
+    //            null,
+    //            1000,
+    //            int.MaxValue);
+    //        //用SqlDependency Call BroadcastShape
 
-        }
-
-
-
-        private Random random = new Random();
+    //    }
 
 
-        private void BroadcastShape(object state)
-        {
-            // 定期執行的方法
-            _hubContext.Clients.All.sendTest1(random.Next(1000).ToString());
-        }               
-        public static Broadcaster Instance
-        {
-            get
-            {
-                return _instance.Value;
-            }
-        }
-    }
+
+    //    private Random random = new Random();
+
+
+    //    private void BroadcastShape(object state)
+    //    {
+    //        // 定期執行的方法
+    //        _hubContext.Clients.All.sendTest1(random.Next(1000).ToString());
+    //    }               
+    //    public static Broadcaster Instance
+    //    {
+    //        get
+    //        {
+    //            return _instance.Value;
+    //        }
+    //    }
+    ////}
 }
